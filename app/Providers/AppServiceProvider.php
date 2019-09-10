47c626a7809f631796051d28d5b113a418950266
kajal.garg <kajal.garg@saffrontech.net>
2019-09-10 11:06:47 +0530

    Eliminated Expected cost box from patient

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,11 +22,6 @@ class AppServiceProvider extends ServiceProvider{
      * @return void
      */
     public function boot(UrlGenerator $url){
-<<<<<<< HEAD
-        $url->forceScheme('https');
-=======
-        //$url->forceScheme('https');
->>>>>>> d90d22caf04b0821bf4d48cff2ed22720bba8c92
         Schema::defaultStringLength(191);
     }
 }
