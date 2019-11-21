c369b7aaa1e318cb4d27769b4f4fb78bd2c3c182
shokeenn <sonu.shokeen@saffrontech.net>
2019-11-21 16:11:14 +0530

    Resolved bug

--- app/Http/Controllers/API/v1/BookingController.php
+++ app/Http/Controllers/API/v1/BookingController.php
@@ -509,12 +509,13 @@ class BookingController extends Controller
                 $data = unserialize($value['weekdays']);
                 $bookings[$key]['weekdays'] = $data;
             }
-
-            $bookings[$key]['caregivers']['name'] = $value->userCaregiver->name;
-            $bookings[$key]['caregivers']['profile_image'] = $value->userCaregiver->profile_image == null ? 'default.png' : $value->userCaregiver->profile_image ;
-            $bookings[$key]['caregivers']['language'] = $value->userCaregiver->language;
-            $bookings[$key]['caregivers']['description'] = $value->userCaregiver->description;
-            $bookings[$key]['caregivers']['discipline'] = Qualification::select('name')->join('caregiver_attributes' ,'caregiver_attributes.value' , 'qualifications.id')->where('type' , 'qualification')->where('caregiver_id', $value->userCaregiver->id)->get()->toArray();
+            if($value->userCaregiver != null){
+                $bookings[$key]['caregivers']['name'] = $value->userCaregiver->name;
+                $bookings[$key]['caregivers']['profile_image'] = $value->userCaregiver->profile_image == null ? 'default.png' : $value->userCaregiver->profile_image ;
+                $bookings[$key]['caregivers']['language'] = $value->userCaregiver->language;
+                $bookings[$key]['caregivers']['description'] = $value->userCaregiver->description;
+                $bookings[$key]['caregivers']['discipline'] = Qualification::select('name')->join('caregiver_attributes' ,'caregiver_attributes.value' , 'qualifications.id')->where('type' , 'qualification')->where('caregiver_id', $value->userCaregiver->id)->get()->toArray();
+            }
         }
 
         if(count($bookings) > 0){
