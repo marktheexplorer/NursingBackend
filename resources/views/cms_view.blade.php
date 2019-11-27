93fa597f72a5cb935328151450cde66fa4491f66
kajal.garg <kajal.garg@saffrontech.net>
2019-11-27 16:43:37 +0530

    Change privacy policy design

--- resources/views/cms_view.blade.php
+++ resources/views/cms_view.blade.php
@@ -2,17 +2,11 @@
 
 @section('content')
 @if($cms)
-	<div class="jumbotron text-center pb-1">
-		<img src="{{ asset('mail/email-logo.png') }}" style="width: 590px;">
-	  	<h1>{{ $cms->title }}</h1>
-	  	<hr>
-	</div>
 	<div class="container">
 	  	{!! $cms->content !!}
 	</div>
 @else
 	<div class="jumbotron text-center">
-		<img src="{{ asset('mail/email-logo.png') }}" style="width: 590px;">
 	  	<h1>Page content not found.</h1>
 	  	<hr>
 	</div>
