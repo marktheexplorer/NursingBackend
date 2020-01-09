b24cfac0d728ffabb3b44e5dd61659e9ab01b93d
kajal.garg <kajal.garg@saffrontech.net>
2020-01-09 12:18:48 +0530

    Work on Time picker

--- resources/views/bookings/manageBooking.blade.php
+++ resources/views/bookings/manageBooking.blade.php
@@ -82,7 +82,7 @@
                              <div class="card-header" style="background-color: #ddd;">
                                 <h5>Manage Booking</h5>
                              </div>
-                             <div class="tab-content row">
+                             <!-- <div class="tab-content row">
                                 <div class="tab-pane fade show active col-md-12" id="tab-2">
                                   <div class="card-body">
                                     <div class="managebookingWrap">
@@ -125,7 +125,45 @@
                                     </div>
                                  </div>
                                 </div>
-                             </div>
+                             </div> -->
+
+                             <div class="container-fluid cloned-row1 educat_info" id="cloned-row1" name="cloned-row1">
+                                <div class="row">
+                                    <input type="hidden" name="booking_id" value="{{$booking->id}}">
+                                    <div class="row form-group">
+                                        <label class="col-md-3">Choose CareGiver</label>
+                                        <div class="col-md-9">                                           
+                                            <select name="caregiver[]" class="form-control select2 caregiver" id="caregiver">
+                                                @foreach($caregivers as $caregiver)
+                                                    <option value="{{ $caregiver->id }}" >{{ $caregiver->user->name .' ('. $caregiver->user->email . ')'}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>   
+                                    <div class="row form-group">
+                                        <label class="col-md-3"> Date</label>
+                                        <div class="col-md-9">                                           
+                                            <input type="text" id="start_data" class="form-control floating-label ipt_Field  start_data deg_date" placeholder="Start Date" style="max-width: 220px;float: left;"  name="start_data[]" >   
+                                            <span style="display: inline;float: left;margin: 7px 30px;font-weight: 600;">To </span>                                 
+                                            <input type="text" id="end_date" class="form-control floating-label deg_date" placeholder="End Date" style="max-width: 220px;float: left; " name="end_date[]" >      
+                                        </div>
+                                    </div>                           
+                                    <div class="row form-group">
+                                        <label class="col-md-3">Time</label>
+                                        <div class="col-md-9">     
+                                            <input type="text" id="start_time" class="form-control floating-label deg_time" placeholder="Start Time" style="max-width: 220px;float: left;" name="start_time[]">
+                                            <span style="display: inline;float: left;margin: 7px 30px;font-weight: 600;">To </span>
+                                            <input type="text" id="end_time" class="form-control floating-label deg_time" placeholder="End Time" style="max-width: 220px;float: left;" name="end_time[]">  
+                                        </div>
+                                    </div>
+                                </div> 
+                                <!-- Third row-fluid-->
+                                <div class="row">
+                                   <div class="col-xs-12 addBtnWrap">
+                                        <a class="btn btn-info add_button" ><i class="fas fa-plus"></i> Add</a>
+                                    </div>
+                                </div>
+                            </div>
                            </div>
                            <div class="row">
                             <div class="form-group col-sm-5 pull-right"></div>
@@ -149,10 +187,47 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.min.css" rel="stylesheet" />
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
 
 <script>
+
+    $(document).ready(function () { 
+    var currentDate = new Date();
+        $(".deg_date").datepicker();
+        $(".start_time").timepicker();
+
+            var count=0;
+            $(document).on("click", ".add_button", function () { 
+                var $clone = $('.cloned-row1:eq(0)').clone(true,true);
+                //alert("Clone number" + clone);
+                $clone.find('[id]').each(function(){this.id+='someotherpart'+count});
+                $clone.find('.btn_more').after("<input type='button' class='btn_less1' value='less' id='buttonless'/>")
+                $clone.attr('id', "added"+(++count));
+                $clone.find("input.deg_date")
+                          .removeClass('hasDatepicker')
+                          .removeData('datepicker')
+                          .unbind()
+                          .datepicker();
+                $clone.find("input.start_time")
+                          .removeAttr("id")
+                          .attr('id','start_time'+count)
+                          .unbind()
+                          .timepicker();
+
+                $("#start_time"+count).timepicker();
+                $(this).parents('.educat_info').after($clone);
+                ++count;
+            });
+            $(document).on('click', ".btn_less1", function (){
+                var len = $('.cloned-row1').length;
+                if(len>1){
+                    $(this).closest(".btn_less1").parent().parent().parent().remove();
+                }
+            });
+        });
                                                         
-    $("[id^=startDate]").bootstrapMaterialDatePicker({
+    /*$("[id^=startDate]").bootstrapMaterialDatePicker({
         format : 'MM/DD/YYYY',
         time: false ,
         minDate : new Date(),
@@ -204,7 +279,7 @@
 
     $('.managebookingWrapItem .delBtn').click(function(){
         $(this).parent().parent().remove();
-    });
+    });*/
 
  </script>
 @endsection
