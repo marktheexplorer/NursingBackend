3dd8b2b1a58dc0f79f2fac82a46dd123d6b77f06
kajal.garg <kajal.garg@saffrontech.net>
2020-02-21 12:08:25 +0530

    Changed un-block to unblock

--- resources/views/diagnose/index.blade.php
+++ resources/views/diagnose/index.blade.php
@@ -47,7 +47,7 @@
                                         @if($diagnose->is_blocked)
                                             <button type="button" class="btn-sm btn-danger btn-cir" title="Block"><i class="fas fa-lock"></i></button>
                                         @else
-                                            <button type="button" class="btn-sm btn-success btn-cir" title="Un-Block"><i class="fas fa-lock-open"></i></button>
+                                            <button type="button" class="btn-sm btn-success btn-cir" title="Unblock"><i class="fas fa-lock-open"></i></button>
                                         @endif
                                         </a>
                                     </li>
